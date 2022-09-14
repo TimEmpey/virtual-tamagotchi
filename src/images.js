134c831ac17746980997b625b161a174fe810bc8
Tim Empey <timothyempey@gmail.com>
2022-09-13 19:53:11 -0700

    re-organize html

--- src/images.js
+++ src/images.js
@@ -1,7 +1,17 @@
-import tom1 from './images/tom-holland.png';
-import tom2 from './images/tom-pickles.png';
-import tom3 from './images/tom-selleck.png';
-import tom4 from './images/tom-hardy.png';
+import tom1 from './images/tom-engine.png';
+import tom2 from './images/tom-brady.png';
+import tom3 from './images/tom-cruise.png';
+import tom4 from './images/tom-felton.png';
+import tom5 from './images/tom-hanks.png';
+import tom6 from './images/tom-hardy.png';
+import tom7 from './images/tom-hiddelston.png';
+import tom8 from './images/tom-holland.png';
+import tom9 from './images/tom-jerry.png';
+import tom10 from './images/tom-omally.png';
+import tom11 from './images/tom-pickles.png';
+import tom12 from './images/tom-riddle.png';
+import tom13 from './images/tom-selleck.png';
+import tom14 from './images/tom-tucker.png';
 
-let myPix = [tom1, tom2, tom3, tom4];
+let myPix = [tom1, tom2, tom3, tom4, tom5, tom6, tom7, tom8, tom9, tom10, tom11, tom12, tom13, tom14];
 export default myPix;
